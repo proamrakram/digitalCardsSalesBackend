f70417387bd60cb0faf3f624daa6073cc98b7382
proamrakram <amroaarsi@gmail.com>
2026-02-12 00:04:37 +0200

    new updates

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         api: __DIR__ . '/../routes/api.php',
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
-        apiPrefix: '/api/backend/'
+        apiPrefix: 'backend'
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
